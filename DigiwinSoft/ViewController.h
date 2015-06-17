0c39bb8cfa88663dfbd4893f8c0d7d6c68e93739
JyunYan-Li <mo700602@gmail.com>
2015-06-17 17:00:47 +0800

    create project

---
+++ DigiwinSoft/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DigiwinSoft
+//
+//  Created by Jyun on 2015/6/17.
+//  Copyright (c) 2015年 Jyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DigiwinSoft/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DigiwinSoft
+//
+//  Created by Jyun on 2015/6/17.
+//  Copyright (c) 2015年 Jyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
