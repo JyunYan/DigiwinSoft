67580ab49a64624e1faafc8e50faf063d21d2060
kenn <kenn.chang@msa.hinet.net>
2015-06-19 00:26:05 +0800

    test push

--- DigiwinSoft/AppDelegate.m
+++ DigiwinSoft/AppDelegate.m
@@ -19,7 +19,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    
+    //123
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 
     self.tabBarController = [[MTabBarViewController alloc] init];
