23575b77e4e5c39f5c5c9c27fe3478b24b106e3f
JyunYan-Li <mo700602@gmail.com>
2015-06-29 17:57:53 +0800

    1. 更新資料庫 2. 添加iCarousel View (未完)

---
+++ DigiwinSoft/utilities/MConfig.h
@@ -0,0 +1,15 @@
+//
+//  MConfig.h
+//  DigiwinSoft
+//
+//  Created by Jyun on 2015/6/29.
+//  Copyright (c) 2015年 Jyun. All rights reserved.
+//
+
+#ifndef DigiwinSoft_MConfig_h
+#define DigiwinSoft_MConfig_h
+
+#define DEVICE_SCREEN_WIDTH [[UIScreen mainScreen] bounds].size.width
+#define DEVICE_SCREEN_HEIGHT [[UIScreen mainScreen] bounds].size.height
+
+#endif
