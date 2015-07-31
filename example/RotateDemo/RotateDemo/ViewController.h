06865a22747c6d727615295b3eac0969641c75ee
justin <justin@amisogosftware.com.tw>
2015-07-31 10:58:39 +0800

    增加選轉文字Control範例..

---
+++ example/RotateDemo/RotateDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  RotateDemo
+//
+//  Created by meowhoo on 2015/7/30.
+//  Copyright (c) 2015年 amigosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CircularTextView.h"
+
+@interface ViewController : UIViewController<CircularTextViewDelegate>
+
+@end
